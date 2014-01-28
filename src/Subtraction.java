6aea47e1d8af9d87651921ee7ca17c1778d7e3a7
Henrik <johanshe>
2014-01-28 09:32:51 +0100

    Threads

---
+++ src/Subtraction.java
@@ -0,0 +1,22 @@
+import java.lang.Thread;
+
+public class Subtraction extends Thread
+{
+	public Subtraction()
+	{
+		
+	}
+	
+	public void run()
+	{
+		System.out.println("Subtraction started");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Subtraction finished");
+	}
+
+}
